5cc344f0a7e918a0543a02911c6201819dc047f4
Jason Hornbuckle <jshornbuckle@gmail.com>
2015-09-05 11:29:28 -0400

    Set up unit tests for TrackerService and write 2

--- tests/unit/TrackerService.spec.js
+++ tests/unit/TrackerService.spec.js
@@ -4,6 +4,7 @@ describe('TrackerService', function () {
 		$interpolate,
 		TrackerService,
 		TRACKER_URL,
+		testDates,
 		testUrl;
 
 	beforeEach(module('kargo'));
@@ -15,21 +16,11 @@ describe('TrackerService', function () {
 		TrackerService = $injector.get('TrackerService');
 		TRACKER_URL = $injector.get('TRACKER_URL');
 		testUrl = $interpolate(TRACKER_URL);
+		testDates = [
+			{ from: '2015-01-01', to: '2015-01-02' },
+			{ from: '2015-01-01', to: '2015-01-03' }
+		];
 		
-
-		$httpBackend.when('GET', testUrl('2015-01-01', '2015-03-20'))
-			.respond({ data: [
-				{
-					id: 1,
-					hits: 9271,
-					date: "2015-01-01"
-				},
-				{
-					id: 7,
-					hits: 4125,
-					date: "2015-01-02"
-				}
-			]});
 
 	}));
 
@@ -45,11 +36,65 @@ describe('TrackerService', function () {
 	});
 
     it('should return data', function () {
-    	$httpBackend.expectGET(testUrl('2015-01-01', '2015-03-20'));
-    	var hits = TrackerService.getHits('2015-01-01', '2015-03-20');
+
+    	$httpBackend.when('GET', testUrl({
+    		fromDate: testDates[0].from,
+    		toDate: testDates[0].to
+    	})).respond({ data: [
+			{
+				id: 1,
+				hits: 9271,
+				date: "2015-01-01"
+			},
+			{
+				id: 7,
+				hits: 4125,
+				date: "2015-01-02"
+			}
+		]});
+
+    	$httpBackend.expectGET(testUrl({
+    		fromDate: testDates[0].from,
+    		toDate: testDates[0].to
+    	}));
+    	
+    	TrackerService.getHits(testDates[0].from, testDates[0].to).then(function (response) {
+    		expect(response.data).toBeDefined();
+    	});
+
     	$httpBackend.flush();
-    	console.log(hits);
-    	//expect
+    });
+
+    it('should add a dummy data point if one date is missing', function () {
+
+    	$httpBackend.when('GET', testUrl({
+    		fromDate: testDates[1].from,
+    		toDate: testDates[1].to
+    	})).respond({ data: [
+			{
+				id: 1,
+				hits: 9271,
+				date: "2015-01-01"
+			},
+			{
+				id: 7,
+				hits: 4125,
+				date: "2015-01-03"
+			}
+		]});
+
+		TrackerService.getHits(testDates[1].from, testDates[1].to).then(function (response) {
+			var addedData = response.data[1];
+
+    		expect(response.data.length).toBe(3);
+    		expect(addedData.hits).toBe(0);
+    		expect(addedData.date).toBe('2015-01-02');
+    		expect(addedData.id).toBeDefined();
+
+    	});
+
+    	$httpBackend.flush();
+
     });
 
 
